af19d35cd7b75efd2a0d6165594012de8663ccce
jefYamba <joph.e.f.yamba@gmail.com>
2026-05-20 14:28:46 +0300

    add logging to ResendEmailService and update S3 endpoint configuration

--- src/main/kotlin/dev/jefy/connectpro/shared/infrastructure/messaging/ResendEmailService.kt
+++ src/main/kotlin/dev/jefy/connectpro/shared/infrastructure/messaging/ResendEmailService.kt
@@ -5,6 +5,8 @@ import com.resend.services.emails.model.CreateEmailOptions
 import dev.jefy.connectpro.shared.infrastructure.config.ResendEmailProperties
 import dev.jefy.connectpro.shared.infrastructure.messaging.strategy.EmailStrategy
 import dev.jefy.connectpro.user.domain.vo.Email
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
 
@@ -20,6 +22,7 @@ class ResendEmailService(
     val emailProperties: ResendEmailProperties
 ) : EmailService {
     private val resend = Resend(emailProperties.apiKey)
+    private val logger: Logger = LoggerFactory.getLogger(ResendEmailService::class.java)
     override fun send(email: Email, strategy: EmailStrategy) {
         try {
             val request = CreateEmailOptions.builder()
@@ -30,7 +33,8 @@ class ResendEmailService(
                 .build()
 
             resend.emails().send(request)
-        } catch (_: Exception) {
+        } catch (e: Exception) {
+            logger.error("Resend API error: ${e.message}", e)
             throw EmailNotSentException()
         }
     }
